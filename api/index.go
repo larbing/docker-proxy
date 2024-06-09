4ae19e98cde54e27ebac637c46a0a22e8065099a
larbing <larbing@163.com>
2024-06-09 07:21:37 +0000

    up

---
+++ api/index.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	// "fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	targetURL, err := url.Parse("https://registry-1.docker.io/")
+	if err != nil {
+		http.Error(w, "目标服务器返回错误", 500)
+		return
+	}
+
+	// fmt.Fprintf(w, targetURL.String())
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+	r.URL.Host = targetURL.Host
+	r.URL.Scheme = targetURL.Scheme
+	r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+	r.Host = targetURL.Host
+	proxy.ServeHTTP(w, r)
+}
