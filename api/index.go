faf96e0dba98a12b34edbc6399c84dd53d26ad41
larbing <larbing@163.com>
2024-06-12 08:27:20 +0000

    up

--- api/index.go
+++ api/index.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	// "fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -14,9 +15,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "目标服务器返回错误", 500)
 		return
 	}
-
+	log.Printf("%s %s %s %s", r.RemoteAddr, r.Method, r.URL, r.Header)
 	// fmt.Fprintf(w, targetURL.String())
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
 	r.URL.Host = targetURL.Host
 	r.URL.Scheme = targetURL.Scheme
 	r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
