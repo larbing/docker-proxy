58ad102cb1c42ca2d9bc1c0ece32cfddae05e79d
larbing <larbing@163.com>
2024-06-12 09:50:37 +0000

    up

--- api/index.go
+++ api/index.go
@@ -11,12 +11,18 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	targetURL, err := url.Parse("https://registry-1.docker.io/")
+	proxyURL := "https://registry-1.docker.io/"
+
+	if strings.Contains(r.URL.Path,"registry-v2") {
+		proxyURL = "https://production.cloudflare.docker.com/"
+	}
+
+	targetURL, err := url.Parse(proxyURL)
 	if err != nil {
 		http.Error(w, "目标服务器返回错误", 500)
 		return
 	}
-	log.Printf("%s %s %s %s", r.RemoteAddr, r.Method, r.URL, r.Header)
+	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
 	// fmt.Fprintf(w, targetURL.String())
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 
@@ -28,7 +34,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		locationHeader := r.Header.Get("Location")
 		if locationHeader != "" {
 			url, _ := url.Parse(locationHeader)
-			locationHeader = strings.Replace(locationHeader,url.Host, "docker.1panel.live", -1)
+			locationHeader = strings.Replace(locationHeader,url.Host, "docker.sonainai.com", -1)
 			r.Header.Set("Location", locationHeader)
 			log.Printf("Location: %s", locationHeader)
 		}
